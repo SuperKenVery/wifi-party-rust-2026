2f5b4cd1ac35312a79d6143c40f2d62b64653a97
ken <39673849+SuperKenVery@users.noreply.github.com>
2026-06-24 20:16:12 +0800

    fix: Add x86 android target in rust toolchain

--- flake.nix
+++ flake.nix
@@ -35,10 +35,8 @@
         extensions = ["rust-src" "rust-analyzer"];
         targets = [
           "wasm32-unknown-unknown"
-          "aarch64-linux-android"
-          "aarch64-apple-ios"
-          "aarch64-apple-ios-sim"
-          "x86_64-apple-ios"
+          "aarch64-linux-android" "x86_64-linux-android"
+          "aarch64-apple-ios" "aarch64-apple-ios-sim" "x86_64-apple-ios"
         ];
       });
 
